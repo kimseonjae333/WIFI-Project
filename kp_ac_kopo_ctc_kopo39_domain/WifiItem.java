8b09b847e7ed81c2c30620b1f10045c69135c16e
SEONJAE KIM <79892278+kimseonjae333@users.noreply.github.com>
2023-08-11 10:20:38 +0900

    Add files via upload

---
+++ kp_ac_kopo_ctc_kopo39_domain/WifiItem.java
@@ -0,0 +1,123 @@
+package kp_ac_kopo_ctc_kopo39_domain;
+
+public class WifiItem {
+	//도메인 작업 전부 다 하기
+	private int id;
+	private String installationLocationName; //설치장소명
+	private String installationLocationDetails; //설치장소상세
+	private String installationCityName; //설치도시명
+	private String installationDistrictName; //설치시군구명
+	private String installationFacilityType; //설치시설구분
+	private String serviceProviderName; //서비스제공사명
+	private String wifiSsid; //와이파이SSID
+	private String dateOfInstallation; //설치연월
+	private String roadNameAddress; //소재지도로명주소
+	private String lotNumberAddress; //소재지지번주소
+	private String managementAgencyName; //관리기관명
+	private String managementAgencyPhoneNumber; //관리기관전화번호
+	private double latitude; //위도
+	private double longitude; //경도
+	private String created; //데이터기준일자
+	public int getId() {
+		return id;
+	}
+	public String getInstallationLocationName() {
+		return installationLocationName;
+	}
+	public String getInstallationLocationDetails() {
+		return installationLocationDetails;
+	}
+	public String getInstallationCityName() {
+		return installationCityName;
+	}
+	public String getInstallationDistrictName() {
+		return installationDistrictName;
+	}
+	public String getInstallationFacilityType() {
+		return installationFacilityType;
+	}
+	public String getServiceProviderName() {
+		return serviceProviderName;
+	}
+	public String getWifiSsid() {
+		return wifiSsid;
+	}
+	public String getDateOfInstallation() {
+		return dateOfInstallation;
+	}
+	public String getRoadNameAddress() {
+		return roadNameAddress;
+	}
+	public String getLotNumberAddress() {
+		return lotNumberAddress;
+	}
+	public String getManagementAgencyName() {
+		return managementAgencyName;
+	}
+	public String getManagementAgencyPhoneNumber() {
+		return managementAgencyPhoneNumber;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setInstallationLocationName(String installationLocationName) {
+		this.installationLocationName = installationLocationName;
+	}
+	public void setInstallationLocationDetails(String installationLocationDetails) {
+		this.installationLocationDetails = installationLocationDetails;
+	}
+	public void setInstallationCityName(String installationCityName) {
+		this.installationCityName = installationCityName;
+	}
+	public void setInstallationDistrictName(String installationDistrictName) {
+		this.installationDistrictName = installationDistrictName;
+	}
+	public void setInstallationFacilityType(String installationFacilityType) {
+		this.installationFacilityType = installationFacilityType;
+	}
+	public void setServiceProviderName(String serviceProviderName) {
+		this.serviceProviderName = serviceProviderName;
+	}
+	public void setWifiSsid(String wifiSsid) {
+		this.wifiSsid = wifiSsid;
+	}
+	public void setDateOfInstallation(String dateOfInstallation) {
+		this.dateOfInstallation = dateOfInstallation;
+	}
+	public void setRoadNameAddress(String roadNameAddress) {
+		this.roadNameAddress = roadNameAddress;
+	}
+	public void setLotNumberAddress(String lotNumberAddress) {
+		this.lotNumberAddress = lotNumberAddress;
+	}
+	public void setManagementAgencyName(String managementAgencyName) {
+		this.managementAgencyName = managementAgencyName;
+	}
+	public void setManagementAgencyPhoneNumber(String managementAgencyPhoneNumber) {
+		this.managementAgencyPhoneNumber = managementAgencyPhoneNumber;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	
+	
+	//getterr,setter 생성
+	
+	
+	
+}	
